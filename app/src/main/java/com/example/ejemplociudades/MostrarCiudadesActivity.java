9c7f96b45226c541f59866d6105163fea3699d53
joseljg <joseljg@gmail.com>
2021-01-22 17:52:38 +0100

    He creado el recyclerview y el item layout

---
+++ app/src/main/java/com/example/ejemplociudades/MostrarCiudadesActivity.java
@@ -0,0 +1,14 @@
+package com.example.ejemplociudades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MostrarCiudadesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostrar_ciudades);
+    }
+}
